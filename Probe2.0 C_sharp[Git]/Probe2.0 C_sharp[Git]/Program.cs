f82debecb296e4519ede183e2849ba0c1af8924d
KarDan02 <02kardan@rgtfo-me.it>
2020-04-16 10:53:59 +0200

    Zentrierte Ausgabe

--- Probe2.0 C_sharp[Git]/Probe2.0 C_sharp[Git]/Program.cs
+++ Probe2.0 C_sharp[Git]/Probe2.0 C_sharp[Git]/Program.cs
@@ -9,20 +9,36 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Titel("Dies ist ein Test");
+            
+
+            Titel("Dies ist ein Test", ConsoleColor.Red);
+            Console.WriteLine("ttt");
             Console.ReadKey();
         }
-        static void Titel(string titel)
+        static void Titel(string titel, ConsoleColor textfarbe)
         {
+            int breite = Console.WindowWidth;
+            int textlen = titel.Length;
+            ConsoleColor aktTextFarbe = Console.ForegroundColor;
+            Console.Clear();
+            Console.ForegroundColor = textfarbe;
             for (int i = 0; i < Console.WindowWidth; i++)
             {
                 Console.Write("*");
             }
+
+            for (int i = 0; i < (breite-textlen)/2; i++)
+            {
+                Console.Write(" ");
+            }
+
             Console.WriteLine(titel);
+
             for (int i = 0; i < Console.WindowWidth; i++)
             {
                 Console.Write("*");
             }
+            Console.ForegroundColor = aktTextFarbe;
         }
     }
 }
