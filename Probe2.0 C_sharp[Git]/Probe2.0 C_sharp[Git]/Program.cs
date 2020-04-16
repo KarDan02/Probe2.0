6b74f9185c00264959a38808143b78cc7befc3aa
KarDan02 <02kardan@rgtfo-me.it>
2020-04-16 10:33:15 +0200

    Programmbeschreibung

--- Probe2.0 C_sharp[Git]/Probe2.0 C_sharp[Git]/Program.cs
+++ Probe2.0 C_sharp[Git]/Probe2.0 C_sharp[Git]/Program.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+//*** Konvertierung in Zahlensysteme
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
 
             Console.ReadKey();
         }
