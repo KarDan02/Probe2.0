bf75411469a895dc79bf6b938f127f9b17ca1739
KarDan02 <02kardan@rgtfo-me.it>
2020-04-16 10:41:04 +0200

    TitelMethode erstellt und getestet

--- Probe2.0 C_sharp[Git]/Probe2.0 C_sharp[Git]/Program.cs
+++ Probe2.0 C_sharp[Git]/Probe2.0 C_sharp[Git]/Program.cs
@@ -9,7 +9,20 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Titel("Dies ist ein Test");
             Console.ReadKey();
+        }
+        static void Titel(string titel)
+        {
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(titel);
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
         }
     }
 }
